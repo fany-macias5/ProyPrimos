df8b9c674984fa2ecc7e01e99af23674abdff547
fany.macias <fanyroblesma@gmail.com>
2023-06-22 22:18:24 -0600

    Primer commit

--- primosFor.py
+++ primosFor.py
@@ -1,4 +1,17 @@
+#Alessandra Estefanía Robles Macías
 try:
-  n = input("Escribe un número: ")
+  n = int(input("Escribe un número: "))
 except ValueError:
   n = 1
+#En este punto el programa corria pero primero me imprimia más de una vez si era un número primo, luego ya no imprimia nada
+#Así que me puse a investigar un poco más de porque pasaba esto y también investigue un poco más sobre que funciones podría agregar al código
+#Por lo que a partir de esta parte cambie varias cosas, hasta que el programa finalmente compilo de la forma que se requería
+if n > 1:
+   for i in range(2, n):
+       if n % i == 0:
+         print ("NO")
+         break
+   else:
+    print("SI")
+else:
+    print("NO")
--- primosWhile.py
+++ primosWhile.py
@@ -1,4 +1,20 @@
+#Alessandra Estefanía Robles Macías
 try:
-  n = input("Escribe un número: ")
+  n = int(input("Escribe un número: "))
 except ValueError:
-  n = 1
+ n = 1
+#De igual forma que en el código de for tuve que elimar unas líneas y modificarlas por otras solo que en este casao usando while
+"""En este código me atore porque me decía que n no estaba definida, y no podía encontrar el error y después me di cuenta que en algun momento
+había eliminado el valor de la variable n, en cuanto volví a escribirlo el programa ejecuto correctamente
+"""
+if n > 1:
+    i = 2
+    while i < n:
+        if n % i == 0:
+           print("NO")
+           break
+        i += 1
+    else:
+        print("SI")
+else:
+   print("NO")
